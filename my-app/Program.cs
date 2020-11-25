1cbf5307782e02a44bc3b8bc5c5c7d89f624ecbc
Damir <damiralyukov12345@gmail.com>
2020-11-25 10:24:08 +0300

    Added new func GetCurrentDate in Main.cs

--- my-app/Program.cs
+++ my-app/Program.cs
@@ -4,9 +4,14 @@ namespace my_app
 {
     public class Program
     {
+        public static string GetCurrentDate(string? format)
+        {
+            return DateTime.Now.ToString(format);
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetCurrentDate(null));
         }
     }
 }
